336e9d63e04cfd92c2eec6341b2e801b2d65d0a8
Audrey Wright <audrey@hirelofty.com>
2023-07-07 16:56:45 -0500

    initialize app and lots of wip

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { NavBar, BaseAlert } from '@/components'
+import { useAuthStore } from '@/stores'
+
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <div class="app-container" :class="authStore.user && 'bg-light'">
+    <NavBar />
+    <BaseAlert />
+    <div class="container pt-4 pb-4">
+      <router-vew />
+    </div>
+  </div>
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
